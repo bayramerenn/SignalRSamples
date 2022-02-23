e549575e3b81d36e73078d6dd13fd8f69281aef2
Bilgiislem Bayram Eren <bayram.eren@outlook.com.tr>
2022-02-24 01:16:54 +0300

    Add project files.

---
+++ SignalRSample.API/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSample.API.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRSample.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> Index(int teamCount)
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"Arkadaslar takim {teamCount} kisi olacaktir.");
+            return Ok();
+        }
+    }
+}
---
+++ SignalRSample.API/Hubs/MyHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRSample.API.Hubs
+{
+    public class MyHub:Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+        private static int ClientCount { get; set; }
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiceNames",Names);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount",ClientCount);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
